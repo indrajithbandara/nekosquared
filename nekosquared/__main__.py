96f6b06e2c8585ad05e11683e5f17527df22a9a4
Espy <deltamimikyu@outlook.com>
2018-02-04 14:47:26 +0000

    Switched to YAML

--- nekosquared/__main__.py
+++ nekosquared/__main__.py
@@ -4,6 +4,6 @@ Application entry point.
 from nekosquared.engine import bot
 from nekosquared.shared import config
 
-cfg_file = config.get_config_data('discord.ini')
+cfg_file = config.get_config_data('discord.yaml')
 
 bot.Bot(cfg_file()).run()
